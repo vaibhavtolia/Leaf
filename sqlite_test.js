ee2eb74783ef802b034d8306dad33c957ce171aa
Vaibhav Tolia <vaibhavtolia@gmail.com>
2013-06-11 16:10:39 +0530

    initial commit

---
+++ sqlite_test.js
@@ -0,0 +1,23 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('test1');
+
+db.serialize(function() {
+	console.log(db);
+	db.run("CREATE TABLE lorem (info TEXT)");
+
+	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+	for (var i = 0; i < 10; i++) {
+	  stmt.run("lorem " + i);
+	}
+	stmt.finalize();
+
+	db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+	  console.log(row.id + ": " + row.info);
+	});
+
+	db.each("SELECT rowid AS id, info FROM loremipsum", function(err, row) {
+	  console.log(row.id + ": " + row.info);
+	});
+});
+
+db.close();
