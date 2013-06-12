b79ec37a29463417d3d72dbb26e42bcf54e1ac8c
Vaibhav Tolia <vaibhavtolia@gmail.com>
2013-06-12 12:22:46 +0530

    data-feed client and server made, directory structure update, added modules socket.io-client

---
+++ devices/data_feed.server.js
@@ -0,0 +1,17 @@
+var io = require('socket.io').listen(2001);
+
+io.sockets.on('connection', function (socket) {
+	
+	setInterval(function(){
+		var data_string = generateDeviceData();
+		socket.emit('data-feed',data_string);
+	},5000);
+
+});
+
+function generateDeviceData(){
+	var time = 5*60;
+	var session_id = "x0a0a1234";
+	var energy_consumption = Math.floor((Math.random()*10)+1);
+	return "session_id="+session_id+"&energy_consumption="+energy_consumption+"&time="+time
+}
---
+++ middleware/clients/data_feed.client.js
@@ -0,0 +1,19 @@
+var query_string = require('querystring'),
+	 io = require('socket.io-client');
+
+socket = io.connect('localhost', {
+    port: 2001
+});
+
+socket.on('connect', function () { 
+	console.log("socket connected"); 
+});
+
+socket.on('data-feed',function(data){
+	storeConsumptionData(data);
+})
+
+function storeConsumptionData(data){
+	var feed_data = query_string.parse(data);
+	console.log(feed_data);
+}
